d8ab8e8d3c64028b966903a8b64c40f7238f8894
Tran Nam Son <trannamson0328@gmail.com>
2025-11-17 00:52:15 +0700

    hosted backend(2 ENV): switch betwwen dev & prod env here

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+// Toggle this manually for testing
+const isDev = true;
+
 // backend port
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: isDev
+    ? "http://localhost:3001" // Development
+    : "https://naver-lens.onrender.com/", // Production
 });
+
 // instance.defaults.withCredentials = true;
 
 export default instance;
