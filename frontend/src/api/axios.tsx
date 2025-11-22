a05bd0aa150ac5e8b7fb96053e2d110229f73614
wheevu <101315525+wheevu@users.noreply.github.com>
2025-11-22 16:21:23 +0700

    chore: config files for naver cloud deployment

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,13 +1,20 @@
 import axios from "axios";
 
+<<<<<<< HEAD
 // Toggle this manually for testing
 const isDev = true;
+=======
+// Automatically detect environment using Vite
+// true when running 'npm run dev', false when running 'npm run build'
+const isDev = import.meta.env.DEV;
+>>>>>>> 78fb5be (chore: config files for naver cloud deployment)
 
 // backend port
 const instance = axios.create({
   baseURL: isDev
     ? "http://localhost:3001" // Development
-    : "http://49.50.137.153" //"https://naver-lens.onrender.com", // Production
+    : "http://49.50.137.153", // Production (Naver Cloud)
+  // Previous Render link for reference: "https://naver-lens.onrender.com"
 });
 
 // instance.defaults.withCredentials = true;
